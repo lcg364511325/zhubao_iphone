502b014995e9e9915477f8a78f3a96cde2a6af3a
shenyuanmengyi <316146650@qq.com>
2014-09-16 09:57:12 +0800

    初始化

---
+++ zhubao_iphone/zhubao_iphone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  zhubao_iphone
+//
+//  Created by johnson on 14-9-16.
+//  Copyright (c) 2014年 SUNYEARS___FULLUSERNAME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ zhubao_iphone/zhubao_iphone/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  zhubao_iphone
+//
+//  Created by johnson on 14-9-16.
+//  Copyright (c) 2014年 SUNYEARS___FULLUSERNAME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
